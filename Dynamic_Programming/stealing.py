c364ea4045cbf03755d2fde0dce46dd7a3d04330
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-02 00:20:49 +0900

    bug: (완료) 동적계획법 문제4 풀이

--- Dynamic_Programming/stealing.py
+++ Dynamic_Programming/stealing.py
@@ -4,8 +4,8 @@ def solution(money):
     getLast = [0] * len(money) # 첫번째 집 말고 마지막 집을 턴 경우
     
     getFirst[0] = money[0]
+    getFirst[1] = money[0] # 이 부분을 넣어줌으로써 테스트1번 통과. 여기가 0으로 두면 안됌.
     getLast[1] = money[1]
-    
     
     for i in range(2, len(money)-1):
         getFirst[i] = max(getFirst[i-1], getFirst[i-2]+money[i])
